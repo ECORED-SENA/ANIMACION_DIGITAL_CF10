81f0ac643da164184c85c80690d663b21a64ae60
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2022-02-14 16:03:25 -0500

    banner

---
+++ src/components/BannerPrincipalC.vue
@@ -0,0 +1,137 @@
+<template lang="pug">
+.banner-principal
+  
+  .container.degrade.tarjeta
+    
+    .row.banner-principal__row(
+      :style="{'background-image': globalData.fondoBannerPrincipal ? `url(${globalData.fondoBannerPrincipal})` : 'none'}"
+    )
+      .col-lg-7.col-xxl-5.ps-4.ps-sm-5.py-4.py-sm-5.banner-principal__info
+        .banner-principal__componente
+          h1.mb-0(v-html="globalData.componenteFormativo")
+        .banner-principal__descripcion
+          p.mb-0(v-html="globalData.descripcionCurso")
+        .banner-principal__accion
+          router-link.boton(:to="{name: iniciarLnk.nombreRuta }")
+            span.me-1 Ver más
+            i.fas.fa-angle-right
+
+      .d-none.d-lg-block.col-lg-5.px-0.banner-principal__img
+        .contenedor-imagenes
+    
+          img(:src="globalData.imagenBannerPrincipal")            
+          .flotante_1: img(src="@/assets/curso/portada/flotante1.png")
+          .flotante_2: img(src="@/assets/curso/portada/flotante2.png")
+
+                                  
+          
+</template>
+
+<script>
+import mixins from '../components/plantillaMixins'
+export default {
+  name: 'BannerPrincipal',
+  mixins: [mixins],
+  computed: {
+    globalData() {
+      return this.$config.global
+    },
+  },
+}
+</script>
+
+<style lang="sass">
+
+.banner-principal__row
+  background-size: cover
+  background-position: center
+
+.banner-principal
+  p, h1, h2, h3, h4, h5, h6
+    color: $color-banner-text
+
+  .tarjeta
+    background-color: $color-banner-fondo
+    background-size: cover
+    background-position: center
+
+  &__info
+    display: flex
+    flex-direction: column
+    justify-content: center
+
+  &__programa
+    display: flex
+    align-items: center
+    margin-bottom: 20px
+
+  &__componente
+    margin-bottom: 20px
+    h1
+      line-height: 1.1em
+
+      @media (max-width: $bp-max-xs)
+        font-size: 2em
+
+  &__descripcion
+    margin-bottom: 20px
+
+  &__row
+    @if $banner-principal-img-x == 'derecha'
+      justify-content: space-between
+    @else
+      justify-content: space-around
+      .banner-principal__img
+        padding-right: 1.5rem!important
+      @media (min-width: $bp-min-sm)
+        .banner-principal__img
+          padding-right: 3rem!important
+
+  &__img
+    @if $banner-principal-img-y == 'arriba'
+      align-self: flex-start
+      padding-bottom: 1.5rem
+      @media (min-width: $bp-min-sm)
+        padding-bottom: 3rem!important
+    @else if $banner-principal-img-y == 'abajo'
+      align-self: flex-end
+      padding-top: 1.5rem
+      @media (min-width: $bp-min-sm)
+        padding-top: 3rem!important
+    @else
+      align-self: center
+      padding-top: 1.5rem
+      padding-bottom: 1.5rem
+      @media (min-width: $bp-min-sm)
+        padding-top: 3rem!important
+        padding-bottom: 3rem!important
+
+.contenedor-imagenes
+  position: relative
+
+.flotante
+  &_1
+    animation: float2 2s ease-in-out infinite
+    position: absolute
+    top: 0%
+    left: 0%
+    width: 100%
+.flotante
+  &_2
+    animation: float2 4s ease-in-out infinite
+    position: absolute
+    top: 0%
+    left: 0%
+    width: 100%
+
+
+
+
+@keyframes float2
+  0%
+    opacity: 0
+  50%
+    opacity: 1
+  100%
+    opacity: 0
+</style>
